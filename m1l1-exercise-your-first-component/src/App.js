b2312368804d993d54f25282eb8b5ae98c65f2f9
belajar <bljkmp@gmail.com>
2023-03-24 04:35:00 +0700

    Your First Component template

---
+++ m1l1-exercise-your-first-component/src/App.js
@@ -0,0 +1,10 @@
+function App() { 
+  return ( 
+    <div className="App"> 
+      This is the starting code for "Your first component" ungraded lab 
+      <Heading /> 
+    </div> 
+  ); 
+} 
+ 
+export default App;
