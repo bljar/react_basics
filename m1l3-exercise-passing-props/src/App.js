c3d7b9529e8853172e802aee39460d3503a929f9
belajar <bljkmp@gmail.com>
2023-03-24 13:27:09 +0700

    Passing props exercise template

---
+++ m1l3-exercise-passing-props/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="John" />
+            <Heading firstName="Bob"/>
+        </div>
+    );
+};
+
+export default App;
