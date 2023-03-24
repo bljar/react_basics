c1a7d6cb29bd68f484f51f214fd5812840d9f796
belajar <bljkmp@gmail.com>
2023-03-24 13:28:08 +0700

    completed passing prop exercise

--- m1l3-exercise-passing-props/src/App.js
+++ m1l3-exercise-passing-props/src/App.js
@@ -3,8 +3,8 @@ import Heading from "./Heading";
 function App() {
     return (
         <div className="App">
-            <Heading firstName="John" />
             <Heading firstName="Bob"/>
+            <Heading firstName="John"/>
         </div>
     );
 };
