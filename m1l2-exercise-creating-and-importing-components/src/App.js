2bf7b831e3980ef5c280d837bc8019bbec420ac3
belajar <bljkmp@gmail.com>
2023-03-24 09:59:52 +0700

    move Heading function to Heading.js

--- m1l2-exercise-creating-and-importing-components/src/App.js
+++ m1l2-exercise-creating-and-importing-components/src/App.js
@@ -1,13 +1,6 @@
-function Heading() {
-  return (
-    <h1></h1>
-  )
-}
-
 function App() {
   return (
     <div className="App">
-      This is the starting code for "Your first component" ungraded lab
       <Heading />
     </div>
   );
