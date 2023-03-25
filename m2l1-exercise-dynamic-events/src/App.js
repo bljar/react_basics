653bcdcae3a530046c68111db450d45e230ddfa9
belajar <bljkmp@gmail.com>
2023-03-25 09:37:46 +0700

    create a button

--- m2l1-exercise-dynamic-events/src/App.js
+++ m2l1-exercise-dynamic-events/src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div>
       <h1>Task: Add a button and handle a click event</h1>
       <button>
-        
+        Guess the number between 1 and 3.
       </button>
     </div>
   );
