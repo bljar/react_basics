d3afee4fd1e77c0f754b8ef2593b448fbd62f9a3
belajar <bljkmp@gmail.com>
2023-03-27 08:19:30 +0700

    edit button name

--- m4l1-graded-build-a-calculator-app/src/App.js
+++ m4l1-graded-build-a-calculator-app/src/App.js
@@ -58,8 +58,8 @@ function App() {
         <button onClick={minus}>subtract</button>
         <button onClick={times}>multiply</button>
         <button onClick={divide}>divide</button>
-        <button onClick={resetInput}>reset Input</button>
-        <button onClick={resetResult}>reset Result</button> 
+        <button onClick={resetInput}>reset input</button>
+        <button onClick={resetResult}>reset result</button> 
       </form> 
     </div> 
   ); 
